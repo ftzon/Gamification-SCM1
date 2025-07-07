ccbe907feef8e268cab42a1d1ee5bdd7233cc42b
ftzon <163707498+ftzon@users.noreply.github.com>
2025-07-07 09:38:20 +0200

    Add files via upload
    Added scripts and data files

---
+++ SCM1_survey.R
@@ -0,0 +1,24 @@
+## Dependencies
+library(scales)
+library(dplyr)
+#library(summarytools)
+library("likert")
+## https://ladal.edu.au/tutorials/surveys/surveys.html
+
+##Read and clean data set
+df <- read.csv("HvA TFC TTC_dataset.csv", header=TRUE,)
+data <- df[, 3:10]
+colnames(data) <-c("Q1","Q2","Q3","Q4","Q5","Q6","Q7","Q8")
+
+cols <- colnames(data)
+data[cols] <- lapply(data[cols], factor,levels=c(1,2,3,4,5),labels=c("Strongly disagree","Disagree","Neutral","Agree", "Strongly agree"))
+
+png("results/student-survey.png", height = 7, width = 7, uni ='in', res=300)
+plot(likert(data), ordered = F, wrap = 60)
+dev.off()
+
+
+
+
+
+
